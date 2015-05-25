336f9712b2cd4ea6b4279b3e50a61046fb8cc4c4
Talal Abou Haiba <talal.abouhaiba@gmail.com>
2015-05-24 20:47:39 -0700

    Added print to file functionality

--- Main.java
+++ Main.java
@@ -11,6 +11,7 @@
 
 */
 
+import java.io.PrintWriter;
 import java.util.*;
 
 import org.jsoup.Jsoup;
@@ -18,18 +19,18 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-        import java.io.IOException;
-        import java.util.List;
+import java.io.IOException;
 
 
-        public class Main {
 
-            public static void main(String[] args) {
+public class Main {
 
-                int zip;
-                char sort_type; // user input for sort type
+    public static void main(String[] args) {
 
-        List<Tax_Return_Preparer> taxPreparers = new ArrayList<Tax_Return_Preparer>();
+        int zip;
+        char sort_type; // user input for sort type
+
+        List<Tax_Return_Preparer> taxPreparers = new ArrayList<>();
 
 
         Scanner in = new Scanner(System.in);
@@ -43,11 +44,15 @@ import org.jsoup.select.Elements;
             }
 
             zip = in.nextInt();
-        } while (zip <= 99 || zip >= 99999); // valid zip is 5 digits, however the IRS website allows searches using the first three digits. lowest searchable zip code is therefore '100'
+        }
+        while (zip <= 99 || zip >= 99999); // valid zip is 5 digits, however the IRS website allows searches using the first three digits. lowest searchable zip code is therefore '100'
 
 
-        System.out.println("You entered: " +zip);
+        System.out.println("You entered: " + zip);
+
         try {
+
+            PrintWriter writer = new PrintWriter("Tax Preparers.txt");
 
             Document doc = Jsoup.connect("http://search.irs.gov/search?q=" + zip + "&site=efile&client=efile_frontend&output=xml_no_dtd&proxystylesheet=efile_frontend&filter=0&getfields=*&partialfields=buszip%3A" + zip + "%7Czip_w%3A" + zip + "&num=1000").get();
             String title = doc.title();
@@ -67,9 +72,9 @@ import org.jsoup.select.Elements;
 
                 // Replace all non-breaking space with whitespace
 
-                Name = Name.replaceAll( "\u00a0", " ");
-                Address = Address.replaceAll( "\u00a0", " ");
-                City_State_Zip = City_State_Zip.replaceAll( "\u00a0", " ");
+                Name = Name.replaceAll("\u00a0", " ");
+                Address = Address.replaceAll("\u00a0", " ");
+                City_State_Zip = City_State_Zip.replaceAll("\u00a0", " ");
                 Point_of_contact = Point_of_contact.replaceAll("\u00a0", " ");
 
 
@@ -85,7 +90,7 @@ import org.jsoup.select.Elements;
             System.out.println("Found a total of: " + taxPreparers.size() + " results. \n");
 
             System.out.println("Please select sort type: [L]ast name or [P]hone number");
-                sort_type = in.next().charAt(0);
+            sort_type = in.next().charAt(0);
 
             while (sort_type != 'l' && sort_type != 'L' && sort_type != 'P' && sort_type != 'p') {
 
@@ -96,36 +101,41 @@ import org.jsoup.select.Elements;
 
             if (sort_type == 'l' || sort_type == 'L') { // sort and display by last name
 
-                    System.out.println("Sorting by last name:");
+                System.out.println("Sorting by last name:");
 
-                    Collections.sort(taxPreparers, Tax_Return_Preparer.compareByName);
+                Collections.sort(taxPreparers, Tax_Return_Preparer.compareByName);
 
-                    System.out.println("\nContact Name, Number, Business, Address, City/State/Zip;\n ");
+                System.out.println("\nContact Name, Number, Business, Address, City/State/Zip;\n ");
+                writer.println("\nContact Name, Number, Business, Address, City/State/Zip;\n ");
 
-                    for (Tax_Return_Preparer t: taxPreparers){
-                        System.out.println(t.getFullName() + ", " + t.getTelephone() + ", " + t.getName_Of_Business() + ", " + t.getAddress() + ", " + t.getCity_State_Zip() + "; " );
-                    }
+
+                for (Tax_Return_Preparer t : taxPreparers) {
+                    System.out.println(t.getFullName() + ", " + t.getTelephone() + ", " + t.getName_Of_Business() + ", " + t.getAddress() + ", " + t.getCity_State_Zip() + "; ");
+                    writer.println(t.getFullName() + ", " + t.getTelephone() + ", " + t.getName_Of_Business() + ", " + t.getAddress() + ", " + t.getCity_State_Zip() + "; ");
                 }
-
-
-            if (sort_type == 'p' || sort_type == 'P') { // sort and display by phone number
-                    System.out.println("Sorting by phone number:");
-
-                    Collections.sort(taxPreparers, Tax_Return_Preparer.compareByPhone);
-
-                    System.out.println("\nNumber, Contact Name, Business, Address, City/State/Zip;\n ");
-
-                    for (Tax_Return_Preparer t: taxPreparers) {
-                        System.out.println(t.getTelephone() + ", " + t.getFullName() + ", " + t.getName_Of_Business() + ", " + t.getAddress() + ", " + t.getCity_State_Zip() + "; " );
-                    }
             }
 
 
-        }
+            if (sort_type == 'p' || sort_type == 'P') { // sort and display by phone number
+                System.out.println("Sorting by phone number:");
 
-        catch(IOException e) {
+                Collections.sort(taxPreparers, Tax_Return_Preparer.compareByPhone);
+
+                System.out.println("\nNumber, Contact Name, Business, Address, City/State/Zip;\n ");
+                writer.println("\nNumber, Contact Name, Business, Address, City/State/Zip;\n ");
+
+                for (Tax_Return_Preparer t : taxPreparers) {
+                    System.out.println(t.getTelephone() + ", " + t.getFullName() + ", " + t.getName_Of_Business() + ", " + t.getAddress() + ", " + t.getCity_State_Zip() + "; ");
+                    writer.println(t.getTelephone() + ", " + t.getFullName() + ", " + t.getName_Of_Business() + ", " + t.getAddress() + ", " + t.getCity_State_Zip() + "; ");
+                }
+            }
+
+            System.out.println("\n The tax preparers can be found in 'Tax Preparers.txt' ");
+
+        } catch (IOException e) {
             System.out.print("An error has occurred. Exiting.");
             e.printStackTrace();
         }
     }
 }
+
